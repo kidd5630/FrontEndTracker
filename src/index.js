a771d7bf703089b26bff9055cd20dcf521201441
John <jvt703@gmail.com>
2021-10-11 00:08:24 -0400

    background color update

--- src/index.js
+++ src/index.js
@@ -77,7 +77,7 @@ const App = () => {
 
 				{userToken
 				?
-				(<div>
+				(<div className="backdrop">
 					<Switch>	
 						<Route exact path ="/">
 							<Home 
@@ -146,7 +146,7 @@ const App = () => {
 					</Switch>
 				</div>)	
 				: 
-				(<div >
+				(<div className="backdrop" >
 					<Switch>
 						<Route exact path ="/">
 							<Home 
@@ -222,7 +222,6 @@ const App = () => {
 				</div>)
 				}
 				
-				<div className="backdrop"></div>	
 				
 			
 			</div>
